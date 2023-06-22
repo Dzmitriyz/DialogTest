9f9d825cfaf90e31f3e1e311743fac99d8e45f4d
hekto2004@mail.ru <235113Qwe>
2023-06-22 23:10:35 +0700

    DialogFragment

---
+++ app/src/main/java/com/example/dialogtest/MyDialog.java
@@ -0,0 +1,31 @@
+package com.example.dialogtest;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+
+public class MyDialog extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle saveInstanceState){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setMessage("Make a select")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                return builder.create();
+
+    }
+}
